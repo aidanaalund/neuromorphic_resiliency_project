4bcd688e1dc05285c79855afb0a22d69b8c06170
aidanaalund <aidan.aalund@utexas.edu>
2025-04-18 11:27:37 -0500

    Started new way to validate

--- validation_scripts/gsc_validation.py
+++ validation_scripts/gsc_validation.py
@@ -2,6 +2,15 @@ import torch
 import torch.nn as nn
 import snntorch as snn
 from snntorch import surrogate
+import os
+import sys
+
+# import sanafe stuff
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+#PROJECT_DIR = os.path.abspath((os.path.join(SCRIPT_DIR, os.pardir)))
+PROJECT_DIR = '/home/ala4225/sana-fe'
+sys.path.insert(0, PROJECT_DIR)
+import sanafe
 
 # Citation: Network definition from neurobench Google Speech Commands example
 spike_grad = surrogate.fast_sigmoid()
@@ -42,7 +51,7 @@ def forward(self, x):
 
     # Layer 4
     y = self.linear4(spk3)
-    spk4, mem4 = self.lif4(y)   
+    spk4, mem4 = self.lif4(y) 
     
     return spk4, mem4
 
@@ -55,55 +64,13 @@ if __name__ == "__main__":
 
     # First, load the sequential model state dict to inspect it
     sequential_state = torch.load(r"/home/ala4225/neurobench/neurobench/examples/gsc/model_data/s2s_gsc_snntorch",map_location=torch.device('cpu'))
-    # Print the keys to see the layer names
-    for key in sequential_state.keys():
-        print(key)
-    # This might print something like:
-    # 0.weight
-    # 0.bias
-    # 1.weight
-    # 1.bias
-    # etc.
-
-    sequential_to_manual = {
-        '1.weight' : 'flat1.weight',
-        '1.bias' : 'flat1.bias',
-        '2.threshold': 'lif1.threshold',
-        '2.graded_spikes_factor' : 'lif1.graded_spikes_factor',
-        '2.reset_mechanism_val' : 'lif1.reset_mechanism_val',
-        '2.beta' : 'lif1.beta',
-        '3.weight' : 'linear1.weight',
-        '3.bias' : 'linear1.bias',
-        '4.threshold' : 'lif2.threshold',
-        '4.graded_spikes_factor' : 'lif2.graded_spikes_factor',
-        '4.reset_mechanism_val' : 'lif2.reset_mechanism_val',
-        '4.beta' : 'lif2.beta',
-        '5.weight' : 'linear2.weight',
-        '5.bias' : 'linear2.bias',
-        '6.threshold' : 'lif3.threshold',
-        '6.graded_spikes_factor' : 'lif3.graded_spikes_factor',
-        '6.reset_mechanism_val' : 'lif3.reset_mechanism_val',
-        '6.beta' : 'lif3.beta',
-        '7.weight' : 'linear3.weight',
-        '7.bias' : 'linear3.bias',
-        '8.threshold' : 'lif4.threshold',
-        '8.graded_spikes_factor' : 'lif4.graded_spikes_factor',
-        '8.reset_mechanism_val' : 'lif4.reset_mechanism_val',
-        '8.beta' : 'lif4.beta'
-    }
 
 
 
-    new_state_dict = {}
-    for old_key, param in sequential_state.items():
-        if old_key in sequential_to_manual:
-            new_key = sequential_to_manual[old_key]
-            new_state_dict[new_key] = param
+    # time step of spikes. Doesn't have to be spike perfect but get close. Convert to csv.
     
-    print(new_state_dict.keys())
-    net.load_state_dict(new_state_dict)
+    # Run the snntorch
 
-    # maybe try NIR?
-
-    # convert to yaml
-    # utils.convert_class_snn_to_yaml(net,'gsc',[{'rate': 0.6}])
+    # Run the sanafe stuff
+    # look at dvs_gesture.py in sanafe's scripts folder
+    
